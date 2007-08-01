190c70f6c6dd265d6cf1d9b4821283b5a6b50f01
root <root@e770ab43-8333-0410-8350-e2d93f6122ef>
2007-08-01 22:44:53 +0000

    fixed up rails/svn stuff
    git-svn-id: svn://projects.palmettowebconsulting.com/projects/learnstat/trunk@29 e770ab43-8333-0410-8350-e2d93f6122ef

--- public/bundles/qforms/javascripts/qforms_init.js
+++
@@ -1,9 +0,0 @@
-// set the path to the qForms directory
-qFormAPI.setLibraryPath("/bundles/qforms/javascripts/");
-// this loads all the default libraries
-qFormAPI.include("*");
-
-
-/********************************************************************
- load custom validation scripts here
-********************************************************************/
