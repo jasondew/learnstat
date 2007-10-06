c32c3cb5e8d64d2b50a99b53effa07843e4d6586
jasond <jasond@e770ab43-8333-0410-8350-e2d93f6122ef>
2007-10-06 03:29:46 +0000

    polished the notifications
    git-svn-id: svn://projects.palmettowebconsulting.com/projects/learnstat/trunk@65 e770ab43-8333-0410-8350-e2d93f6122ef

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,23 +1,22 @@
 class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
-    @subject    += 'Please activate your new account'
-  
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
-  
+    @recipients  = user.course.instructor.email
+    @subject    += 'learnStat.new activation request'
+    @body[:url]  = "http://learnstat.net/activate/#{user.activation_code}"
   end
   
   def activation(user)
     setup_email(user)
+    @recipients  = user.email
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @body[:url]  = "http://learnstat.net/"
   end
   
   protected
     def setup_email(user)
-      @recipients  = "#{user.email}"
       @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @subject     = "[learnstat.net] "
       @sent_on     = Time.now
       @body[:user] = user
     end
