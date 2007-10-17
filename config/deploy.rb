d0a842fac917b64ed5bcd93ac9458c475c5dd80f
jasond <jasond@e770ab43-8333-0410-8350-e2d93f6122ef>
2007-10-17 00:22:07 +0000

    added mongrel config
    git-svn-id: svn://projects.palmettowebconsulting.com/projects/learnstat/trunk@83 e770ab43-8333-0410-8350-e2d93f6122ef

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repository,  "svn://alpha/projects/learnstat/trunk"
 set :user, "root"
 set :scm_username, "deploy"
 set :scm_password, "C4pisTrano!"
+set :use_sudo, false
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -20,3 +21,31 @@ set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 role :app, "alpha"
 role :web, "alpha"
 role :db,  "alpha", :primary => true
+
+namespace :deploy do
+  namespace :mongrel do
+    [ :stop, :start, :restart ].each do |t|
+      desc "#{t.to_s.capitalize} the mongrel appserver"
+      task t, :roles => :app do
+        #invoke_command checks the use_sudo variable to determine how to run the mongrel_rails command
+        invoke_command "mongrel_rails cluster::#{t.to_s} -C #{mongrel_conf}", :via => run_method
+      end
+    end
+  end
+
+  desc "Custom restart task for mongrel cluster"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    deploy.mongrel.restart
+  end
+
+  desc "Custom start task for mongrel cluster"
+  task :start, :roles => :app do
+    deploy.mongrel.start
+  end
+
+  desc "Custom stop task for mongrel cluster"
+  task :stop, :roles => :app do
+    deploy.mongrel.stop
+  end
+
+end
