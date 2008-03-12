4568576c6d49f5787d53ca0d27329808b1194a18
Jason Dew <jason.dew@gmail.com>
2008-03-12 08:28:32 -0400

    set up vlad for deployment

---
+++ config/deploy.rb
@@ -0,0 +1,6 @@
+set :web, :nginx
+set :scm, :git
+set :application, "learnstat.net"
+set :domain, "learnstat.net"
+set :deploy_to, "/var/www/learnstat.net"
+set :repository, 'http://git@github.com:jasondew/learnstat.git'
