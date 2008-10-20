8be05598c5e7634ba3a7fb635d037678cfff7e98
Jason Dew <jason.dew@gmail.com>
2008-10-20 17:04:17 -0400

    bugfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -71,7 +71,7 @@ class User < ActiveRecord::Base
     @activated = true
     update_attribute :activated_at, Time.now
     UserMailer.deliver_signup_notification self
-    UserMailer.deliver_instructor_signup_notification user
+    UserMailer.deliver_instructor_signup_notification self
   end
 
   def activated?
