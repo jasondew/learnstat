41cb3674ebe31e10f41a690b532a4b79a788f436
Jason Dew <jason.dew@gmail.com>
2008-12-01 20:02:33 -0500

    fixed nil textual datetime

--- lib/textual_date_times.rb
+++ lib/textual_date_times.rb
@@ -2,7 +2,8 @@ module TextualDateTimes
 
   def method_missing(method_name, *args)
     if method_name.to_s =~ /(.*)_textual$/
-      send($1).strftime "%Y-%m-%d %H:%M"
+      time = send($1)
+      time ? time.strftime("%Y-%m-%d %H:%M") : nil
     elsif method_name.to_s =~ /(.*)_textual=$/
       send("#{$1}=", Chronic.parse(args.first))
     else
