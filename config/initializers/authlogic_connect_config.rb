e17c911147bba99fe1af40397b6b578ffe90a187
Jason Dew <jason.dew@gmail.com>
2011-01-02 21:43:59 -0500

    fixing authlogic connect config

--- config/initializers/authlogic_connect_config.rb
+++ config/initializers/authlogic_connect_config.rb
@@ -1,1 +1,23 @@
-AuthlogicConnect.config = YAML.load_file("config/authlogic.yml")
+case Rails.env
+  when "development" then AuthlogicConnect.config = YAML.load_file("config/authlogic.yml")
+  when "production" then
+    AuthlogicConnect.config = {
+      :connect => {
+        :twitter => {
+          :key => ENV["CONNECT_TWITTER_KEY"],
+          :secret => ENV["CONNECT_TWITTER_SECRET"],
+          :label => "Twitter"
+        },
+        :facebook => {
+          :key => ENV["CONNECT_FACEBOOK_KEY"],
+          :secret => ENV["CONNECT_FACEBOOK_SECRET"],
+          :label => "Facebook"
+        },
+        :google => {
+          :key => ENV["CONNECT_GOOGLE_KEY"],
+          :secret => ENV["CONNECT_GOOGLE_SECRET"],
+          :label => "Google"
+        }
+      }
+    }
+end
