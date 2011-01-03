7940aee68e7802eed3da74dfa6654f08f7f3eaaf
Jason Dew <jason.dew@gmail.com>
2011-01-02 21:32:03 -0500

    more fixes

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,7 +3,8 @@ class Document < ActiveRecord::Base
   belongs_to :course
   belongs_to :user
 
-  has_attachment :storage => :file_system
+# FIXME: switch to carrierwave
+#  has_attachment :storage => :file_system
 #  validates_as_attachment
 
   def viewable_now?
