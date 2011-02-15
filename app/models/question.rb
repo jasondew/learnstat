7f1dbe75355531ad0f038e848bdf7ce928d4a4c2
Jason Dew <jason.dew@gmail.com>
2011-02-15 09:12:41 -0500

    adding Question#quick_create

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,6 +5,19 @@ class Question < ActiveRecord::Base
   validates_presence_of :content, :chapter, :answer
   validates_numericality_of :chapter, :answer
 
+  module ClassMethods
+    def quick_create question_text, answer_text, *wrong_answer_texts
+      question = Question.new :content => question_text
+      answer = QuestionChoice.new :content => answer_text
+      wrong_answers = wrong_answer_texts.map {|text| QuestionChoice.new :content => text }
+      [answer, wrong_answers].sort_by { rand }.each {|question_choice| question.choices << question_choice }
+      question.save false
+      question.update_attribute :answer, answer.id
+      question
+    end
+  end
+  extend ClassMethods
+
   def to_s
     content
   end
