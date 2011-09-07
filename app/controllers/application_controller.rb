d79ae100c63e9013fbe598381282081eff2aefcc
Jason Dew <jason.dew@gmail.com>
2011-09-07 10:45:27 -0400

    fixing grade calculation formula

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
 
   def percent_format(number, nil_text="N/A")
     return nil_text unless number
-    number_to_percentage(number * 100, {:precision => 1})
+    number_to_percentage(number * 100, :precision => 0)
   end
 
   def instructor?
