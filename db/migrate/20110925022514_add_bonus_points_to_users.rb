9e5992631c5f5a49013f4244360c84ec0e6159bd
Jason Dew <jason.dew@gmail.com>
2011-09-24 23:40:03 -0400

    fixing migration for ruby < 1.9

--- db/migrate/20110925022514_add_bonus_points_to_users.rb
+++ db/migrate/20110925022514_add_bonus_points_to_users.rb
@@ -1,6 +1,6 @@
 class AddBonusPointsToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :bonus_points, :integer, default: 0
+    add_column :users, :bonus_points, :integer, :default => 0
   end
 
   def self.down
